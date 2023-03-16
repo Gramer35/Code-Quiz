47fff5662f45a292bae0a13b4d59a8c416bf49d3
Chris Gramer <gramer3514@gmail.com>
2023-03-15 20:56:40 -0700

    updated functions and variables

---
+++ assets/script.js
@@ -0,0 +1,73 @@
+// create variables to target id's
+const something = document.getElementById("#quiz");
+
+const time = 60;
+
+// Questions need to be in an array of objects or in HTML? - [0, 1, 2, 3, 4]
+const quizQ = [
+    {
+        "Question": "????",
+        "Answer 1": "Option",
+        "Answer 2": "Option",
+        "Answer 3": "Option",
+        "Answer 4": "Option",
+
+    },
+    {
+        "Question": "????",
+        "Answer 1": "Option",
+        "Answer 2": "Option",
+        "Answer 3": "Option",
+        "Answer 4": "Option",
+
+    },
+    {
+        "Question": "????",
+        "Answer 1": "Option",
+        "Answer 2": "Option",
+        "Answer 3": "Option",
+        "Answer 4": "Option",
+
+    },
+    {
+        "Question": "????",
+        "Answer 1": "Option",
+        "Answer 2": "Option",
+        "Answer 3": "Option",
+        "Answer 4": "Option",
+
+    },
+    {
+        "Question": "????",
+        "Answer 1": "Option",
+        "Answer 2": "Option",
+        "Answer 3": "Option",
+        "Answer 4": "Option",
+
+    },
+];
+
+
+// Create function when submit is selected - needs to start the timer along with push the first question out
+function timer(event) {
+    if (condition) {
+        
+    }
+    
+}
+
+// Create function that will push the first question out and continue to move through questions when selection is made
+
+
+// Create function that will handle the countdown - remove 10 second when answer is wrong - once count down hits 0, needs to determine what to do. Also handle what happens when answers are answered before countdown hits 0
+
+
+// Create a function that will keep track of score/right/wrong answers
+
+// Create function to handle highscores/local storage
+
+
+// Create function to clear after quiz completed and highscore entered. Be able to reset back to the submit section. 
+
+
+// Create function to store and show wins and losses
